8cf1bd48002eb66dfc6bb2dc89c011660f4b3143
dimasputra2020-gif <dimasputra2020@gmail.com>
2025-11-28 08:01:59 +0700

    memindahkan keluar auth

---
+++ app/Http/Controllers/MasterStatusController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\MasterStatus;
+
+class MasterStatusController extends Controller
+{
+    public function index()
+    {
+        $master_status = MasterStatus::all(['code', 'name']);
+
+        return response()->json([
+            'success' => true,
+            'data' => $master_status
+        ]);
+    }
+}
