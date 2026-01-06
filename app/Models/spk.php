1ad08a85e6c3a1b8e095f7b682ca8d4055ad0166
dimasputra2020-gif <dimasputra2020@gmail.com>
2026-01-06 08:56:43 +0700

    merubah beberapa fillable

--- app/Models/spk.php
+++ app/Models/spk.php
@@ -12,32 +12,63 @@ class Spk extends Model
     protected $fillable = [
         'uuid_pengajuan',
         'tanggal',
-        'jenis_pekerjaan',
+        'jenis_pekerjaan_id',
         'kode_barang',
+        'kd_satker',
         'uraian_pekerjaan',
         'stafs',
         'penanggung_jawab_npp',
         'penanggung_jawab_tlp',
         'penanggung_jawab_name',
-        'mengetahui_npp',
-        'mengetahui_name',
+        'penanggung_jawab_ttd',
+        'menyetujui',
+        'menyetujui_name',
+        'menyetujui_npp',
+        'menyetujui_tlp',
+        'menyetujui_ttd',
         'mengetahui',
+        'mengetahui_name',
+        'mengetahui_npp',
+        'mengetahui_tlp',
+        'mengetahui_ttd',
+        'npp_kepala_satker',
         'file',
-        'status',
-        'ttd_mengetahui',
+        'status_id',
         'no_surat',
         'no_referensi',
-        'pelaksana',
-        'pelaksana_npp',
-        'ttd_pelaksana',
+        'kode_barang'
     ];
 
     protected $casts = [
         'stafs' => 'array',
+        'file' => 'array',
+        'timeline' => 'array',
     ];
+
 
     public function pengajuan()
     {
         return $this->belongsTo(Pengajuan::class);
     }
+
+    public function timelines()
+    {
+        return $this->hasMany(Timeline::class, 'uuid_pengajuan', 'uuid_pengajuan')
+            ->orderBy('created_at', 'asc');
+    }
+
+   public function status()
+{
+    return $this->belongsTo(MasterStatus::class, 'status_id');
+}
+
+public function jenisPekerjaan()
+{
+    return $this->belongsTo(
+        \App\Models\MasterJenisPekerjaan::class,
+        'jenis_pekerjaan_id',
+        'id'
+    );
+}
+
 }
