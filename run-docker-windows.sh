efe951487d53b1369663b6782e7eb325b3a1d2b9
angger@3070 <anggerpputro@gmail.com>
2026-01-07 12:26:23 +0700

    add run docker windows

---
+++ run-docker-windows.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+set -e
+
+echo "Copying .env.publik to .env..."
+cp .env.publik .env
+
+echo "Copying docker-compose.windows.yml to docker-compose.yml..."
+cp docker-compose.windows.yml docker-compose.yml
+
+echo "Building docker compose..."
+docker compose up -d --build --remove-orphans --wait --timestamps
+echo "Docker compose built..."
+
+echo "Docker compose running..."
+echo "Waiting for docker compose to finish..."
+wait
+
+echo "Installing composer dependencies..."
+docker compose exec app composer install
+docker compose exec app bash -c "chmod -R 777 storage/"
+docker compose exec app bash -c "chmod -R 777 vendor/"
+docker compose exec app php artisan key:generate --force
+
+echo "Docker compose finished..."
+echo "Docker compose logs:"
+docker compose logs -f --tail=100
